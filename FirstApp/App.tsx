d6cadd04ba9dad7e40439712bc20d67f436918b6
nikoicardo <nikoicardo@gmail.com>
2025-06-26 18:19:23 -0700

    Use the View component.

--- FirstApp/App.tsx
+++ FirstApp/App.tsx
@@ -1,10 +1,13 @@
-import { StyleSheet, SafeAreaView, ActivityIndicator } from "react-native";
+import { StyleSheet, SafeAreaView, View, Text } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <ActivityIndicator />
-      <ActivityIndicator size={"large"} color={"green"} />
+      <View style={styles.view1}>
+        <Text style={styles.text}>Hello</Text>
+        <Text style={styles.text}>Text 2</Text>
+      </View>
+      <View style={styles.view2}></View>
     </SafeAreaView>
   );
 }
@@ -13,5 +16,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+  },
+  view1: {
+    height: 200,
+    width: 200,
+    backgroundColor: "#ff9485",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  view2: {
+    backgroundColor: "#000000",
+    height: 150,
+    width: 150,
+  },
+  text: {
+    fontSize: 50,
   },
 });
